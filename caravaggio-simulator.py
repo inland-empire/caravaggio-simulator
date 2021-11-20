87cec1337c1a462af8b1257dd0d483c4b21a5d01
inland-empire <chrovis@gmail.com>
2021-11-20 17:15:52 +0100

    Make the bartender move (dance)

--- caravaggio-simulator.py
+++ caravaggio-simulator.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 from pygame import Vector2, display
 from pygame.constants import K_LEFT, KEYDOWN, MOUSEBUTTONDOWN, QUIT
 from enum import Enum
@@ -12,9 +13,12 @@ GAME_TITLE = "CARAVAGGIO SIMULATOR"
 SCREEN_HEIGHT = 512
 SCREEN_WIDTH = 608
 
+MARGIN = 50
+
 FPS = 60
 FPS_CLOCK = pygame.time.Clock()
 SPEED = 400
+SCALE = 0.25
 
 # Creates display surface which is essentially the 'window'
 displaySurface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -25,6 +29,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 assets_path = os.path.join(dir_path, "assets")
 background_png_path = os.path.join(assets_path, "tavern.png")
 player_png_path = os.path.join(assets_path, "cara.png")
+bartender_png_path = os.path.join(assets_path, "bartender.png")
 
 
 # Declare key classes - I prefer them in separate files in the future commits
@@ -45,17 +50,11 @@ class Player(pygame.sprite.Sprite):
         img = pygame.image.load(player_png_path)
         img_height = img.get_height()
         img_width = img.get_width()
-        scale = 0.25
-        self.image = pygame.transform.scale(img, (int(img_width * scale), int(img_height * scale)))
+        self.image = pygame.transform.scale(
+            img, (int(img_width * SCALE), int(img_height * SCALE)))
         self.rect = self.image.get_rect()
-
-        # Position and direction
-        self.vx = 0
-        # Player's current position on the surface
-        self.pos = Vector2(340, 240)
         # Player's walking / running speed
         self.velocity = Vector2(0.0, 0.0)
-        self.accelerate = Vector2(0.0)
         # Direction our player is currentlyfacing
         # class Direction(Enum):
         #     UP = 1
@@ -65,14 +64,14 @@ class Player(pygame.sprite.Sprite):
         # self.direction = Direction.RIGHT
 
     def move(self):
-        displacement = self.velocity / FPS 
+        displacement = self.velocity / FPS
         self.rect = self.rect.move(int(displacement.x), int(displacement.y))
 
     def update(self):
-        self.move() # move to new position
-        print(self.velocity)
-        print(self.rect)
-        displaySurface.blit(self.image, self.rect) # draw new player
+        self.move()  # move to new position
+        # print(self.velocity)
+        # print(self.rect)
+        displaySurface.blit(self.image, self.rect)  # draw new player
 
 # This will be the method to make enemy angry and can provoke a fight
     def provoke(self):
@@ -85,10 +84,40 @@ class Player(pygame.sprite.Sprite):
 class Bartender(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        img = pygame.image.load(bartender_png_path)
+        img_height = img.get_height()
+        img_width = img.get_width()
+        self.image = pygame.transform.scale(
+            img, (int(img_width * SCALE), int(img_height * SCALE)))
+        initial_position = Vector2(random.randint(MARGIN, SCREEN_WIDTH - MARGIN),
+                                   random.randint(MARGIN, SCREEN_HEIGHT - MARGIN))
+        self.rect = self.image.get_rect(center=(initial_position))
+
+    def move(self):
+        self.direction = Vector2(random.randint(-1, 1), random.randint(-1, 1))
+        self.velocity = self.direction * SPEED
+        self.displacement = self.velocity / FPS
+        if self.rect.x <= MARGIN:
+            self.displacement.x = MARGIN
+        elif self.rect.x > SCREEN_WIDTH - MARGIN:
+            self.displacement.x = SCREEN_WIDTH - MARGIN
+        if self.rect.y < MARGIN:
+            self.displacement.y = MARGIN
+        elif self.rect.y > SCREEN_HEIGHT - MARGIN:
+            self.displacement.y = SCREEN_HEIGHT - MARGIN
+        self.rect = self.rect.move(
+            int(self.displacement.x), int(self.displacement.y))
+
+    def update(self):
+        self.move()  # move to new position
+        print(self.velocity)
+        print(self.rect)
+        displaySurface.blit(self.image, self.rect)
 
 
 if __name__ == "__main__":
     player = Player()
+    bartender = Bartender()
     background = Background()
 
     running = True
@@ -101,10 +130,12 @@ if __name__ == "__main__":
                 running = False
 
         keys = pygame.key.get_pressed()
-        player.velocity.x = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * SPEED
+        player.velocity.x = (keys[pygame.K_RIGHT] -
+                             keys[pygame.K_LEFT]) * SPEED
         player.velocity.y = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * SPEED
         background.render()
         player.update()
+        bartender.update()
 
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
